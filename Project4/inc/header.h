a1e8b123772fec950ee1c2cd514866e67f9f7d3b
studyrohan <1975849845@qq.com>
2023-07-17 09:00:21 +0800

    dd

--- Project4/inc/header.h
+++ Project4/inc/header.h
@@ -2,3 +2,4 @@
 
 
 int lightTest();
+int start();
--- Project4/src/main.cpp
+++ Project4/src/main.cpp
@@ -5,7 +5,81 @@
 
 
 #include"../inc/header.h"
+using namespace std;
+class base {
+public:
+	void test()
+	{
+		cout << "base test" << std::endl;
+	}
+	base() {
+		cout << "base constructor" << endl;
+		int* b = new int[5];
+	}
+	~base() {
+		cout << "base destructor" << endl;
+		delete[] b;
+	}
+
+private:
+	int* b;
+};
+
+
+
+class derived : public base {
+public:
+	derived() {
+		cout << "derived constructor" << endl;
+		int* d = new int[8];
+	}
+	~derived() {
+		cout << "derived destructor" << endl;
+		delete[] d;
+	}
+	void test()
+	{
+		cout << "derived test" << std::endl;
+	}
+
+private:
+	int* d;
+};
+class A {
+public:
+	A();
+	A(const A& a);
+	A(const A& a,const A&b);
+private:
+	int a;
+};
+A::A()
+	:a(0)
+{
+
+}
+A::A(const A& a)
+{
+	this->a = a.a;
+}
+
+A::A(const A& a, const A& b)
+{
+	this->a = a.a + b.a;
+}
+
+
+
+//int main()
+//{
+//	base* pBase = new derived;
+//	pBase->test();
+//	cout << "---" << endl;
+//	delete pBase;
+//
+//	return 0;
+//}
 int main()
 {
-	lightTest();
+	start();
 }
