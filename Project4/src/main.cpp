17e8bc123fa0a8e53ad17f97544674d9a30cdfc7
studyrohan <1975849845@qq.com>
2024-01-24 18:52:25 +0800

    dd

--- Project4/src/main.cpp
+++ Project4/src/main.cpp
@@ -1,5 +1,8 @@
 
 #include <iostream>
+#include <vector>
+#include <map>
+#include <forward_list>
 #include "glad/include/glad/glad.h"
 #include "GLFW/glfw3.h"
 
@@ -68,7 +71,77 @@ A::A(const A& a, const A& b)
 	this->a = a.a + b.a;
 }
 
+using namespace std;
+struct President
+{
+	std::string name;
+	std::string country;
+	int year;
 
+	President(std::string p_name, std::string p_country, int p_year)
+		: name(std::move(p_name)), country(std::move(p_country)), year(p_year)
+	{
+		cout << "    Construction\n";
+	}
+	President(const President& other)
+		: name(std::move(other.name)), country(std::move(other.country)), year(other.year)
+	{
+		cout << "    Copy Construction\n";
+	}
+	President(President&& other)
+		: name(std::move(other.name)), country(std::move(other.country)), year(other.year)
+	{
+		cout << "    Move Construction\n";
+	}
+};
+int testVector()
+{
+	cout << "emplace_back to vector:\n";
+	vector<President> v1;
+	v1.emplace_back("Nelson Mandela", "South Africa", 1994);
+
+	President tem("daf","asdfsdaf",9);
+	v1.emplace_back(tem);
+
+
+
+	cout << "\npush_back to vector (way-1 - right value reference as parameter):\n";
+	vector<President> v2;
+	v2.push_back(President("Nelson Mandela", "South Africa", 1994));
+
+	cout << "\npush_back to vector (way-2 - left value as parameter):\n";
+	vector<President> v3;
+	President p1("Nelson Mandela", "South Africa", 1994);
+	v3.push_back(p1);
+
+	cout << "\ninsert into map: \n";
+	map<int, President> m1;
+	m1.insert(make_pair<int, President>(10, President("Nelson Mandela", "South Africa", 1994)));
+
+	cout << "\nemplace into map: \n";
+	map<int, President> m2;
+	m2.emplace(10, President("Nelson Mandela", "South Africa", 1994));
+
+
+	std::vector<int>a;
+	int b = 0,c= 1,d =2;
+	a.push_back(b);
+	a.emplace_back(c);
+	return 0;
+}
+void testForwardList()
+{
+	std::forward_list a{ 1,2,3 };
+	
+	a.emplace_front(0);
+	a.push_front(-1);
+	a.emplace_after(a.end(), 8);
+	for (auto item:a)
+	{
+		std::cout << item << ",";
+	}
+	
+}
 
 //int main()
 //{
@@ -81,5 +154,9 @@ A::A(const A& a, const A& b)
 //}
 int main()
 {
-	start();
+	//start();
+	//testVector();
+	testForwardList();
+	system("Pause");
+	return 0;
 }
---
+++ testExportLibDiff/src.cpp
@@ -0,0 +1,7 @@
+#include "head.h"
+int hello()
+{
+	int a = 0;
+	a++;
+	return a;
+}
